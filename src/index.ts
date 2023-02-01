4843226ee2a2cf994dab891a8da42d0355511013
“mayurArde-Dmart” <mayur.arde@dmartlabs.com>
2023-02-01 12:54:54 +0530

    revert to basic lib setup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-export { default as Button } from "./components/Button";
-export { default as Button2 } from "./components/Button2";
-export { default as calculate } from "./utils/calculate";
+export * from "./components";
